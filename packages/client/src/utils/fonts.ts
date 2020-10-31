76372539c607efd56b7a6b39633244af99c508c3
m4gie <steeven.regnault@epitech.eu>
2020-10-31 12:35:22 +0100

    fix(client): load font for mobile

--- packages/client/src/utils/fonts.ts
+++ packages/client/src/utils/fonts.ts
@@ -3,8 +3,8 @@ import { loadAsync } from 'expo-font';
 export default async function loadFonts() {
   try {
     await loadAsync({
-      ZillaSlab_400Regular: require('../../assets/fonts/ZillaSlab-Regular.ttf'),
-      ZillaSlab_500Medium: require('../../assets/fonts/ZillaSlab-Medium.ttf'),
+      OpenSans: require('../../assets/fonts/OpenSans-Regular.ttf'),
+      Oswald: require('../../assets/fonts/Oswald.ttf'),
     });
   } catch (e) {
     console.warn("Can't load font: ", e?.code, e?.message);
